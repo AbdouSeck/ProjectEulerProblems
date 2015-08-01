074b196faeffb05a4df96e8b53cffe1099ccb158
Abdou Seck <djily02016@gmail.com>
2015-08-01 14:36:37 -0400

    Some changes in 37

--- src/Problem35.java
+++ src/Problem35.java
@@ -35,7 +35,7 @@ public class Problem35 {
 	
 	public static boolean isPrime(int num) {
 		if (num > 2 & Math.abs(num)%2 == 0) return false;
-		if (num == 1) return false;
+		if (num == 1 || num == 0) return false;
 		
 		for (int i = 3; i*i <= Math.abs(num); i=i+2) {
 			if(Math.abs(num)%i==0) {
--- src/Problem37.java
+++ src/Problem37.java
@@ -1,9 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class Problem37 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(truncatable(10));
+	}
+	
+	public static boolean truncatable(int number) {
+		if(Problem35.isPrime(number)) {
+			String teststr = Integer.toString(number);
+			int [] truncatedright = new int [teststr.length()];
+			int [] truncatedleft = new int [teststr.length()];
+			for (int j = 0; j < truncatedright.length; j++) {
+				if(Problem35.isPrime(Integer.parseInt(teststr))) {
+					truncatedright[j] = Integer.parseInt(teststr);
+					teststr = teststr.substring(0, teststr.length()-1);
+				} else {
+					truncatedright[j] = 0;
+				}
+			}
+			
+			String teststr1 = Integer.toString(number);
+			for (int j = 0; j < truncatedleft.length; j++) {
+				if(Problem35.isPrime(Integer.parseInt(teststr1))) {
+					truncatedleft[j] = Integer.parseInt(teststr1);
+					teststr1 = teststr1.substring(1, teststr1.length());
+				} else {
+					truncatedleft[j] = 0;
+				}
+			}
+			if(!Arrays.asList(truncatedright).contains(0) & !Arrays.asList(truncatedleft).contains(0)) {
+					return true;
+				}
+		}
+		
+		return false;
 	}
 
 }
