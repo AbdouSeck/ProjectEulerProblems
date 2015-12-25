cb37cfb49f9d3413d48e046e2b371e6130a73b82
Abdou Seck <djily02016@gmail.com>
2015-12-24 19:47:33 -0500

    solved 36 and optmized 39

--- src/Problem36.java
+++ src/Problem36.java
@@ -6,7 +6,6 @@ public class Problem36 {
 		int sum = 0;
 		for (int i = 1; i < 1000000; i++) {
 			if(i == Palindromes.Palindrome(i) & Palindromes.Palindrome(Integer.toBinaryString(i)).equals(Integer.toBinaryString(i))) {
-				System.out.println(i + " and " + Integer.toBinaryString(i));
 				sum += i;
 			}
 		}
--- src/Problem39.java
+++ src/Problem39.java
@@ -4,25 +4,46 @@ import java.util.ArrayList;
 public class Problem39 {
 
 	public static void main(String[] args) {
-			double time1 = System.currentTimeMillis();
-			ArrayList<Integer> ps = new ArrayList<>();
-			for (int i = 10; i <= 1000; i=i+2) {
-				for (int j = 10; j < i; j++) {
-					for (int j2 = 10; j2 < j; j2++) {
-						for (int k = 10; k < j2; k++) {
-							if(k+j2+j==i && k*k+j2*j2-j*j==0) {
-								ps.add(i);
-							}
-						}
+		double time1 = System.currentTimeMillis();
+		ArrayList<Integer> nums = new ArrayList<>();
+		for (int i = 1; i < 1000; i++) {
+			for (int j = 1; j < i; j++) {
+				int a = i*i-j*j;
+				int b = 2*i*j;
+				int c = i*i+j*j;
+				int k = 1;
+				while(k*(a+b+c) <= 1000) {
+					if(Problem3.isPrime(a) || Problem3.isPrime(b) || Problem3.isPrime(c)) {
+						nums.add(k*(a+b+c));
 					}
+					k++;
 				}
 			}
-			for (Integer integer : ps) {
-				System.out.println(integer);
+		}
+		System.out.println(most_common(nums));
+		double time2 = System.currentTimeMillis();
+		System.out.println("The runtime took "+(time2-time1)+" ms.");
+	}
+	
+	public static int most_common(ArrayList<Integer> arrlist) {
+		ArrayList<Integer> temp = new ArrayList<Integer>(arrlist);
+		int common = 0;
+		int largest = 0;
+		
+		for (Integer integer : temp) {
+			int counter = 0;
+			while(arrlist.contains(integer)) {
+				counter++;
+				arrlist.remove(integer);
 			}
-			double time2 = System.currentTimeMillis();
-			System.out.println("The runtime is "+(time2-time1)/1000+" seconds!");
-//			I went ahead and imported and the list created into R to determine the mode of it. That gave me 840.
+			if(counter>largest) {
+				largest = counter;
+				common = integer;
+			}
+		}
+		if(largest == 1) return -1;
+		return common;
+		
 	}
 
 }
